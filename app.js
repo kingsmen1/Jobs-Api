80ded91d81084f28c76b41a0d9c666f57fa33e4e
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-12-21 18:23:19 +0530

    test c5

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ app.use(xss());
 // extra packages
 
 // routes
-app.get("/", (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
+// app.get("/", (req, res) => {
+//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+// });
 app.post("/api/v1/auth/register", async (req, res) => {
   // const { email, name, password } = req.body;
   // if (!email || !name || !password) {
